3ecb17989bf6ab90ef2ad4aca9b91c497dc09273
savva.henchevskyi <savva@global.id>
2022-11-08 11:25:40 +0200

    published doc

--- src/test/crypto-test.ts
+++ src/test/crypto-test.ts
@@ -11,9 +11,8 @@ import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
 //global.window = {};
 //const forge = require("node-forge")
-//import * as CryptoJS from "crypto-js/core";
 
-//const nodeCrypto = require("crypto")
+const nodeCrypto = require("crypto")
 
 
 export let options: Options = {
@@ -22,38 +21,27 @@ export let options: Options = {
 }
 
 
-export default ()=> {
-    sleep(1)
-    const randomUUID = uuidv4()
-    console.log(randomUUID)
-    const privateKeyExample = '-----BEGIN RSA PRIVATE KEY-----\n'+
-        'MIIBOQIBAAJAVY6quuzCwyOWzymJ7C4zXjeV/232wt2ZgJZ1kHzjI73wnhQ3WQcL\n'+
-        'DFCSoi2lPUW8/zspk0qWvPdtp6Jg5Lu7hwIDAQABAkBEws9mQahZ6r1mq2zEm3D/\n'+
-        'VM9BpV//xtd6p/G+eRCYBT2qshGx42ucdgZCYJptFoW+HEx/jtzWe74yK6jGIkWJ\n'+
-        'AiEAoNAMsPqwWwTyjDZCo9iKvfIQvd3MWnmtFmjiHoPtjx0CIQCIMypAEEkZuQUi\n'+
-        'pMoreJrOlLJWdc0bfhzNAJjxsTv/8wIgQG0ZqI3GubBxu9rBOAM5EoA4VNjXVigJ\n'+
-        'QEEk1jTkp8ECIQCHhsoq90mWM/p9L5cQzLDWkTYoPI49Ji+Iemi2T5MRqwIgQl07\n'+
-        'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
-        '-----END RSA PRIVATE KEY-----'
+/* Node-Forge */
+/*
+function generateKeys(): {privateKeyPEM: string, publicKeyPEM: string} {
+    const keypair = forge.pki.rsa.generateKeyPair({bits: 2048, e: 0x10001});
+    const publicKeyPEM = forge.pki.publicKeyToPem(keypair.publicKey);
+    const privateKeyPEM = forge.pki.privateKeyToPem(keypair.privateKey);
+    return { privateKeyPEM, publicKeyPEM }
+}
 
-    /*const keypair = forge.pki.rsa.generateKeyPair({bits: 2048, e: 0x10001});
-    const pubKeyPEM = forge.pki.publicKeyToPem(keypair.publicKey);
-    const privKeyPEM = forge.pki.privateKeyToPem(keypair.privateKey);
-    console.log(pubKeyPEM);
-    console.log(privKeyPEM);
+function createSignature(privateSignKey: string, data: string = "test"){
+    const sign_key = forge.pki.privateKeyFromPem(privateSignKey)
+    const md = forge.md.sha256.create()
+    md.update(data);
+    const signature = sign_key.sign(md)
+    return forge.util.encode64(signature)
+}
+*/
 
-    const enc_key = forge.pki.privateKeyFromPem(privKeyPEM)
-    const hmacDecrypted = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA512("test", randomUUID));
-    console.log(hmacDecrypted)
-
-    const sign_key = forge.pki.privateKeyFromPem(privKeyPEM)
-    const md = forge.md.sha256.create();
-    md.update(hmacDecrypted);
-    const signature = sign_key.sign(md);
-    console.log(forge.util.encode64(signature))*/
-
-
-    /*const { publicKey, privateKey } = nodeCrypto.generateKeyPairSync('rsa',
+/* Node-Crypto */
+function generateKeys(): {privateKeyPEM: string, publicKeyPEM: string} {
+    const { publicKeyPEM, privateKeyPEM } = nodeCrypto.generateKeyPairSync('rsa',
         {
             modulusLength: 2048,  // the length of your key in bits
             publicKeyEncoding: {
@@ -67,10 +55,21 @@ export default ()=> {
                 //passphrase: 'top secret' // *optional*
             }
         })
-    const signature = nodeCrypto.createSign('RSA-SHA256')
-        .update('some data to sign')
-        .sign(privateKey, 'base64');
-    console.log(privateKey)
-    console.log(signature)*/
+    return { privateKeyPEM, publicKeyPEM }
+}
 
+function createSignature(privateKey: string, data: string = "test"){
+    return nodeCrypto.createSign('RSA-SHA256')
+        .update(data)
+        .sign(privateKey, 'base64')
+}
+
+
+export default ()=> {
+    sleep(1)
+    console.log("start")
+    const keys = generateKeys()
+    const signature = createSignature(keys.privateKeyPEM)
+    console.log(signature)
+    console.log("finish")
 }
